08b157e5032a1a83a0a4c8ef85e231c264eabc4f
Andrei Plakhotta <andrei@fireblocks.com>
2021-03-27 16:03:47 +0300

    end day 27.3

--- mmn11/ex1/text_state.c
+++ mmn11/ex1/text_state.c
@@ -4,7 +4,6 @@
 
 #define FALSE (0)
 
-
 enum state {default_state = 0, start_sentence_state, quotation_state, illegal_state};
 
 typedef void (*print_func_t)(int);
@@ -13,8 +12,6 @@ typedef void (*print_func_t)(int);
 /********static functions declaration*****************/
 static enum state NextStatus(enum state current_text_state, int ch);
 
-
-
 static void print_default(int ch);
 static void print_start_sentence(int ch);
 static void print_quotation(int ch);
@@ -22,17 +19,15 @@ static void print_illegal(int ch);
 /********************end static functions declaration****************/
 
 static const print_func_t print_func_arr[] = {&print_default, &print_start_sentence, &print_quotation, &print_illegal};
-
 static enum state g_current_text_state = start_sentence_state; 
 
 
 void ProcessChar(int ch)
 {
-	printf("state enum %d, line %d", g_current_text_state, __LINE__);
-/*	(print_func_arr[g_current_text_state])(ch);*/
+	printf("state enum %d, line %d\n", g_current_text_state, __LINE__);
 	if (!isdigit(ch))
 	{
-			
+		(print_func_arr[g_current_text_state])(ch);		
 	}
 	
 	g_current_text_state = NextStatus(g_current_text_state, ch);
@@ -45,7 +40,11 @@ static enum state NextStatus(enum state current_text_state, int ch)
 	switch (current_text_state)
 	{
 		case start_sentence_state:
-			next_state = default_state;
+			if (!isspace(ch))
+			{
+				next_state = default_state;			
+			}
+			
 			break;
 			
 		case default_state:
@@ -55,13 +54,10 @@ static enum state NextStatus(enum state current_text_state, int ch)
 			break;
 			
 		case illegal_state:
-			
-			assert(0);
 			break;
 	}
 
-	return default_state;	
-/*	return next_state;*/
+	return next_state;
 }	
 
 /**********definitions print by state ***********/
@@ -86,7 +82,6 @@ static void print_quotation(int ch)
 static void print_illegal(int ch)
 {
 	fprintf(stderr, "print_illegal(%c)\n", ch);
-	assert(FALSE);
 }
 
 /*********************************************************/
