b3db4e06a52e426971ce6bc17223b3056e732430
Andrei Plakhotta <andrei@fireblocks.com>
2021-03-27 14:30:23 +0300

    continie ex1 print funcs

--- mmn11/ex1/text_state.c
+++ mmn11/ex1/text_state.c
@@ -2,12 +2,13 @@
 #include <ctype.h> /* */
 #include <assert.h> /*assert()*/
 
+#define FALSE (0)
+
 
 enum state {default_state = 0, start_sentence_state, quotation_state, illegal_state};
 
 typedef void (*print_func_t)(int);
 
-static enum state g_current_text_state = start_sentence_state; 
 
 /********static functions declaration*****************/
 static enum state NextStatus(enum state current_text_state, int ch);
@@ -16,10 +17,14 @@ static enum state NextStatus(enum state current_text_state, int ch);
 
 static void print_default(int ch);
 static void print_start_sentence(int ch);
-
+static void print_quotation(int ch);
+static void print_illegal(int ch);
 /********************end static functions declaration****************/
 
-static const print_func_t print_func_arr[] = {&print_default, &print_start_sentence, NULL, NULL};
+static const print_func_t print_func_arr[] = {&print_default, &print_start_sentence, &print_quotation, &print_illegal};
+
+static enum state g_current_text_state = start_sentence_state; 
+
 
 void ProcessChar(int ch)
 {
@@ -73,7 +78,16 @@ static void print_start_sentence(int ch)
 	putchar(toupper(ch));
 }
 
-
+static void print_quotation(int ch)
+{
+	puts("print_quotation");
+	putchar(toupper(ch));	
+}
+static void print_illegal(int ch)
+{
+	fprintf(stderr, "print_illegal(%c)\n", ch);
+	assert(FALSE);
+}
 
 /*********************************************************/
 
