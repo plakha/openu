026f4e2b9b0ef1d485c715a7dbcf0a8f59299e84
Andrei Plakhota <plahota@gmail.com>
2021-06-07 15:01:43 +0300

    comments

--- mmn23/prnt.c
+++ mmn23/prnt.c
@@ -1,16 +1,45 @@
-#include <stdio.h> 
-#include <assert.h>
+#include <stdio.h> /* putchar() */
+#include <assert.h> /* assert() */
+#include <string.h> /* strncpy() */
 
-void PrintFile(char *file_directory);
+#define BUF_LEN (100)
 
-int main()
+#ifdef __unix__ /* including linux */
+#define EXT ("")
+#else /* windows */
+#define EXT (".exe")
+#endif /* will not work on non-windows, non-unix systems */
+
+/*
+ takes run_file_name that ends with .exe on windows or with no extension on unix/linux and pus te source file name into the buffer src_file_name.
+    the buffer should be initiated to all '\0'. The buffer should have enough size ro hold the file name
+ */
+static void SourceFileName(const char *run_file_name, char *src_file_name);
+
+
+static void PrintFile(char *file_directory);
+
+int main(int argc, char *argv[])
 {
-    PrintFile(__FILE__);
+    char buf[BUF_LEN] = {0};
+    const char *run_file_name = argv[0];
+
+    SourceFileName(run_file_name, buf);
+    PrintFile(buf);
 
     return 0;
 }
 
-void PrintFile(char *file_directory)
+/* the src_file_name has enough size and is filled with '\0' */
+static void SourceFileName(const char *run_file_name, char *src_file_name)
+{
+    const char *c_ext = ".c";
+
+    strncpy(src_file_name, run_file_name, strlen(run_file_name) - strlen(EXT));
+    strcat(src_file_name, c_ext);
+}
+
+static void PrintFile(char *file_directory)
 {
     int c = 0;
     FILE *fd = NULL;
@@ -21,17 +50,19 @@ void PrintFile(char *file_directory)
         fprintf(stderr, "The file %s could not be opened", file_directory);
     }
 
-    while (EOF != (c = fgetc(fd)))
+    while ( EOF != (c = fgetc(fd)) )
     {
         assert (!feof(fd));
 
-        /* Windows editors have new line like this: "\n\r", while Unix/Linux - like this: "\n" */
+        /* Windows editors have new line like this: "\n\r", while Unix/Linux - like this: "\n"
+            when porting windows created text files to linux, the '\r' will not be printed
+         */
         if (c == '\r')
         {
             #ifdef __unix__
                 continue;
             #else
-
+                /* the char will be printed */
             #endif
         }
 
@@ -40,3 +71,6 @@ void PrintFile(char *file_directory)
     
     fclose(fd);
 }
+
+#undef BUF_LEN
+#undef EXT
--- mmn23/prnt.h
+++ mmn23/prnt.h
@@ -1,4 +1,11 @@
 #ifndef PRNT_H
 #define PRNT_H
 
+/*
+The program prints the source file of itself.
+Provided:
+the source file (that ends with ".c") and the executable are in the same directory;
+the executable file ends with no extension on unix/linux , or with ".exe" on Windows.
+*/
+
 #endif /* PRNT_H */
