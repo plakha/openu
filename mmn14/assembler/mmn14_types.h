a951fdc7ab27009d607464a1dc850798987b4e9e
plakha <41244647+plakha@users.noreply.github.com>
2021-08-03 07:53:26 +0300

    Update mmn14_types.h

--- mmn14/assembler/mmn14_types.h
+++ mmn14/assembler/mmn14_types.h
@@ -1,6 +1,10 @@
 #ifndef MMN14_TYPES
 #define MMN14_TYPES
 
+#ifndef MAX_LINE_LEN
+#define MAX_LINE_LEN (81)
+#endif
+
 typedef unsigned char byte;
 
 
@@ -19,4 +23,6 @@ struct word
 }
 word_t;
 
+typedef char[MAX_LINE_LEN] cmd_arg_t;
+typedef cmd_arg[(MAX_LINE_LEN / 2 + 1)] cmd_args_t;
 #endif /* MMN14_TYPES */
