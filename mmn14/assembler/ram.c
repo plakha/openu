352dc7b571298fa266679c42758be0726ba8c7d5
Andrei Plakhota <plakhota@Andreis-MacBook-Pro.local>
2021-08-26 21:23:07 +0300

    RAM implemented, for the second pass

--- mmn14/assembler/ram.c
+++ mmn14/assembler/ram.c
@@ -1,29 +1,84 @@
 #include <stdlib.h> /* malloc */
-#include <assert.h> 
+#include <assert.h> /* assert() */
+#include <stdio.h> /* fprintf() */
 
-#include "mmn14_types.h"
+#include "dvec.h" /* dvec_t */
 
 #include "ram.h"
 
+#define INITIAL_RAM_CAPACITY (1000lu)
+
+const size_t RAM_MAX_SIZE_IN_WORDS = RAM_MAX_SIZE / 4; /* 2^25 Bytes = 2^25/4 Words*/ 
+
 struct ram
 {
-    /* data */
-    int dummy;
-
+    dvec_t *word_arr;
 };
 
 
 ram_t *RAMCreate()
 {
     ram_t *new_ram = malloc(sizeof(*new_ram));
+    if (!new_ram)
+    {
+        fprintf(stderr, 
+        "MEMORY ERROR: could not allocate memory while running line %d in %s/n", 
+        __LINE__, __FILE__);
+    }
+    else{
+        new_ram->word_arr = DVECCreate(sizeof(word_t), INITIAL_RAM_CAPACITY);
+        if (!new_ram->word_arr)
+        {
+            free (new_ram);
+            new_ram = NULL;
+            fprintf(stderr, 
+            "MEMORY ERROR: could not allocate memory while running line %d in %s/n", 
+            __LINE__, __FILE__);
+        }
+    }
 
     return new_ram;
 }
+
+int RAMPushWord(ram_t *ram, const word_t *word)
+{
+    assert(ram);
+    assert(word);
+    assert(ram->word_arr);
+
+    return DVECPushBack(ram->word_arr, word);
+}
+
+word_t *RAMGetIthWord(const ram_t *ram, size_t i)
+{
+    assert(ram);
+    assert(RAMSize(ram) > i);
+    assert(ram->word_arr);
+
+    return (word_t *)DVECGetItemAddress(ram->word_arr, i);
+}
+
+size_t RAMSize(const ram_t *ram)
+{
+    size_t ram_size = -1;
+
+    assert(ram);
+    assert(ram->word_arr);
+
+    ram_size = DVECSize(ram->word_arr);
+
+    assert(RAM_MAX_SIZE_IN_WORDS >= ram_size);
+
+    return ram_size;
+}
+
 
 void RAMDestroy(ram_t *ram)
 {
     assert(ram);
 
+    DVECDestroy(ram->word_arr);
+    ram->word_arr = NULL;
     free(ram);
     ram = NULL;
 }
--- mmn14/assembler/ram.h
+++ mmn14/assembler/ram.h
@@ -1,9 +1,23 @@
 #ifndef RAM_H
 #define RAM_H
 
+#include "mmn14_types.h" /* word_t */
+
+#define RAM_START_ADDRESS (100lu)
+
 typedef struct ram ram_t; 
 
+#define RAM_MAX_SIZE (1lu << 25)
+
+extern const size_t RAM_MAX_SIZE_IN_WORDS;
+
 ram_t *RAMCreate();
+
+int RAMPushWord(ram_t *ram, const word_t *word);
+
+word_t *RAMGetIthWord(const ram_t *ram, size_t i);
+
+size_t RAMSize(const ram_t *ram);
 
 void RAMDestroy(ram_t *ram);
 
